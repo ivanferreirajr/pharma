f3b048d2d98dfca2a25ccd672ad382d7420c8af4
Fellipe Sena <pfosena@gmail.com>
2019-09-08 04:59:35 -0300

    Recebendo dados de cadastro e armazenando-os

--- frontend/src/components/Cadastro.js
+++ frontend/src/components/Cadastro.js
@@ -1,34 +1,43 @@
 import React, { Component } from 'react';
-import bgImg from '../images/tablets-3532308_1920.jpg'
+import bgImg from '../images/tablets-3532308_1920.jpg';
+// import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css'
 
 const divStyle = {
   backgroundImage: `url(${bgImg}) `
 };
 
-class Cadastro extends Component {
-  constructor() {
-    super();
-    this.state = {
-      Email: '',
-      Senha: '',
-      RepSenha: '',
-      Nome: '',
-      DataNascimento: '',
-    };
-    this.onChange = (evento) => {
-      const state = { ...this.state };
-      const campo = evento.target.value;
-      state[campo] = evento.target.value;
-      this.setState(state);
-    };
-    this.cadastrar = this.cadastrar.bind(this);
+
+export default class Cadastro extends Component {
+
+  state = {
+    nome: '',
+    telefone: '',
+    email: '',
+    senha: '',
+    date: ''
   }
 
-  cadastrar() {
+  onInputChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
 
-    // json = {nome:{this.state.Nome},senha:{this.state.Senha}};
+  onChangeDate = date => {
+    this.setState({date});
+  }
 
-    // createUsuario(json)
+  // a variável já armazena os dados em um json (tem que ver como vai pegar os dados de data, que é outro json) e exibe no console do navegador, tudo ok, agora é só pegar esses dados e enviar para o back end, porém não fiz isso porque falta validar a senha, comparar se senha = confSenha, porém está tarde e nao vou fazer isso agora, muito obrigado
+
+  // a tela login é basicamente copiar tudo que tem aqui e colar, já está tudo praticamente pronto
+
+  // ainda nao consegui colocar o background full, dá pra usar alterando o heidth com pixel, porém vai ficar estranho em computadores com tela menores/maioires, nao achei nada a respeito
+
+  onSubmit = e => {
+    console.log(this.state.nome, this.state.senha, this.state.email, this.telefone)
+    e.preventDefault();
   }
 
   render() {
@@ -39,38 +48,77 @@ class Cadastro extends Component {
           <div className="w-35 h-25">
             <div className="card card-body">
               <div id="title"><h4>Cadastro</h4></div>
-              <form>
+              <form onSubmit={this.onSubmit}>
                 <div className="form-group">
-                  <label for="inputName" class="control-label">Nome</label>
-                    <input type="text" class="form-control" id="inputName" placeholder="Digite o seu nome" required></input>
+                  <label className="control-label">Nome</label>
+                    <input 
+                    type="text" 
+                    className="form-control" 
+                    placeholder="Digite o seu nome" 
+                    name="nome"
+                    required
+                    onChange={this.onInputChange}
+                    />
                 </div>
 
-                <div class="form-group">
-                  <label for="inputFone" class="control-label">Telefone</label>
-                  <input type="text" class="form-control" id="inputFone" placeholder="Digite o seu telefone" required></input>         
+                <div className="form-group">
+                  <label className="control-label">Telefone</label>
+                  <input 
+                  type="text" 
+                  className="form-control" 
+                  placeholder="Digite o seu telefone" 
+                  name = "telefone"
+                  required
+                  onChange={this.onInputChange}
+                  />         
                 </div>
 
-                <div class="form-group">
-                  <label for="inputEmail" class="control-label">Email</label>
-                  <input type="email" class="form-control" id="inputEmail" placeholder="Digite o seu email mail" aria-describedby="emailHelp" required></input>
+                <div className="form-group">
+                  <label className="control-label">Email</label>
+                  <input 
+                  type="email" 
+                  className="form-control" 
+                  placeholder="Digite o seu email mail" aria-describedby="emailHelp" 
+                  name="email"
+                  required
+                  onChange={this.onInputChange}
+                  />
                 </div>
 
-                <div class="form-group">
-                  <label for="inputPassword" class="control-label">Senha</label>
-                  <input type="password" class="form-control" id="inputPassword" placeholder="Digite sua senha" required></input>
+                <div className="form-group">
+                  <label className="control-label">Senha</label>
+                  <input 
+                  type="password" 
+                  className="form-control" 
+                  placeholder="Digite sua senha" 
+                  name="senha"
+                  required
+                  onChange={this.onInputChange}
+                  />
                 </div>
 
-                <div class="form-group">
-                  <input type="password" class="form-control" id="inputPassword1" placeholder="Confirme sua senha" required></input>
+                <div className="form-group">
+                  <input 
+                  type="password" 
+                  className="form-control" 
+                  placeholder="Confirme sua senha" 
+                  required
+                  name="confSenha"
+                  />
                 </div>
 
-                <div class="form-group">
-                  <label for="inpudDate" class="control-label">Data de Nascimento</label>
-                  <input type='date' class="form-control" id="inputDate" required ></input>
+                <div className="form-group">
+                  <label htmlFor="inputDate" className="control-label">Data de Nascimento</label>
+                  <DatePicker 
+                  className="form-control"
+                  selected={this.state.date}
+                  required
+                  onChange={this.onChangeDate}
+                  />
                 </div>
 
-                <div class="form-group">
-                  <button type="submit" class="btn btn-primary">Cadastrar-se</button>
+                <div className="form-group">
+                  <button type="submit"className="btn btn-primary">Cadastrar-se</button>
                 </div>
               </form>
             </div>
@@ -81,5 +129,3 @@ class Cadastro extends Component {
     );
   }
 }
-
-export default Cadastro;
